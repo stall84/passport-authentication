616e5fa3bf1dc02512bbe8aba2284fe4a2067793
Michael Stallings <stall84@gmail.com>
2020-05-28 01:59:10 -0400

    continuing to add to app.js structure, added express-session middleware, connect flash and global variable middleware

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
 const mongoose = require('mongoose');
+const flash = require('connect-flash');
+const session = require('express-session');
+
 
 // Database configuration
 const db = require('./config/keys').mongoURI;
@@ -26,10 +29,24 @@ app.set('views', 'views');
 // Express body-parser
 app.use(express.urlencoded({ extended: false }));
 
+// Create the session
+app.use(session({
+  secret: 'butter_butt',
+  resave: true,
+  saveUninitialized: true,
+}))
 
-app.listen(PORT, () => {
-    console.log(`Server UP & RUNNIN on ${PORT}`)
+// Connect Flash middleware
+app.use(flash());
+
+// Global variables
+app.use((req,res,next) => {
+    // apparently this sets up global variables via middleware and res.locals
+    res.locals.success_msg = req.flash('success_msg')
+    res.locals_error_msg = req.flash('error_msg')
+    next();
 })
+
 
 // Routes 
 app.use('/', require('./routes/index'))
@@ -37,3 +54,8 @@ app.use('/users', require('./routes/users'))
 
 // Set up static folder
 app.use(express.static('public'));
+
+// Start/run server
+app.listen(PORT, () => {
+    console.log(`Server UP & RUNNIN on ${PORT}`)
+})
